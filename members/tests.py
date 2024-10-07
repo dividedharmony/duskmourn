21f11575613739a7e23525965f2de7742b4e511b
David Harmon <dharmon@indeed.com>
2024-10-07 16:56:25 -0400

    Add unit test for Member model string representation

--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Member
 
-# Create your tests here.
+class MemberModelTest(TestCase):
+    def test_string_representation(self):
+        member = Member(id=17, firstname='Faye', lastname='King', phone=5551234567, joined_date='2024-10-07')
+        self.assertEqual(str(member), '[017] Faye King')
