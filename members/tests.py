39c88b028bb483d5a29c53315352f5316525311b
David Harmon <dharmon@indeed.com>
2024-10-07 17:09:47 -0400

    feat(tests): Add HomeViewTest and MemberViewTest classes

--- members/tests.py
+++ members/tests.py
@@ -5,3 +5,13 @@ class MemberModelTest(TestCase):
     def test_string_representation(self):
         member = Member(id=17, firstname='Faye', lastname='King', phone=5551234567, joined_date='2024-10-07')
         self.assertEqual(str(member), '[017] Faye King')
+
+class HomeViewTest(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class MemberViewTest(TestCase):
+    def test_when_list_is_empty(self):
+        response = self.client.get('/members/')
+        self.assertEqual(response.status_code, 200)
